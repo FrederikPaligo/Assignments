ebae346edc0ffe05be34e56bca904237167588a8
FrederikPaligo <161586431+FrederikPaligo@users.noreply.github.com>
2026-05-25 16:56:58 +0200

    Update chains.js

--- src/config/chains.js
+++ src/config/chains.js
@@ -1,10 +1,10 @@
 const chains = {
-  defaultChain: [
+  defaultChain: Object.assign([
     { groupId: 1, label: "GAM Author Review", deadline: 30 },
     { groupId: 2, label: "GAM Reviewer Review", deadline: 30 },
-    { groupId: 3, label: "GAM Approver Review", deadline: 30 },
-    { groupId: 4, label: "Auditor Review", deadline: 30 },
-  ],
+    { groupId: 3, label: "GAM Approver Review", deadline: 30, rejectionTarget: 1 },
+    { groupId: 4, label: "Auditor Review", deadline: 30, rejectionTarget: 1 },
+  ], { completionTaxonomyId: 3062 }),
   publicationOverrides: {},
 };
 
